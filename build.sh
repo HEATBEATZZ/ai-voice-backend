1bd18081ab0dc1741756bf76c79cd916c36aae2b
ASHMIT SINGH <heatbeatzz001@gmail.com>
2025-06-13 14:08:23 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,12 @@ apt-get update && apt-get install -y \
     libsdl2-mixer-dev \
     libsdl2-ttf-dev \
     portaudio19-dev \
-    python3-pyaudio
+    python3-pyaudio \
+    build-essential \
+    python3-dev
+
+# Upgrade pip
+pip install --upgrade pip
 
 # Install Python dependencies
 pip install -r requirements.txt 
