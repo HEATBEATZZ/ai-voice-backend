cec867f9de9739b79c451a025722adc401260a2c
ASHMIT SINGH <heatbeatzz001@gmail.com>
2025-06-13 14:04:58 +0530

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install system dependencies
+apt-get update && apt-get install -y \
+    libsdl2-dev \
+    libsdl2-image-dev \
+    libsdl2-mixer-dev \
+    libsdl2-ttf-dev \
+    portaudio19-dev \
+    python3-pyaudio
+
+# Install Python dependencies
+pip install -r requirements.txt 
